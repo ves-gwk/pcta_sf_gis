51d6b754cf4c7f0ac930a7b4102b42455ee5797d
Galen Keily <galenwilliamkeily@gmail.com>
2021-05-27 16:44:04 -0700

    Updated functionality with only 1 start and 1 end possible for each type

--- CoordinatePickerTest.js
+++ CoordinatePickerTest.js
@@ -50,41 +50,128 @@
   // Make it do something on map click
   // mymap.on('click', onMapClick);
   // pct_centerline.on('click', onPCTClick)
-var myIcon = L.divIcon();
+// var myIcon = L.divIcon();
+// // you can set .my-div-icon styles in CSS
+// var start_point = true
+// mymap.on('click', function (e) {
+//     var _select   = document.getElementById("symbols");
+//     var _val      = _select.value;
+//     var layerName = _select.options[_select.selectedIndex].text;
+//     console.log("layer: ", layerName);
+
+//     if (layerName == "PCT Point"){
+//         var pct_marker = L.marker(e.latlng, {
+//         draggable: true
+//         }).addTo(mymap)
+//         var tooltip = L.tooltip()
+//         .setLatLng(e.latlng)
+//         .setContent('<p>Hello world!<br />This is a nice popup.</p>')
+//         .openOn(mymap);
+//         pct_marker.on('click', function(e){
+//             e.target.removeFrom(mymap)
+//         });
+//     }
+//     else{
+//         var side_marker = L.marker(e.latlng, {
+//         draggable: true,
+//         icon: myIcon
+//         }).addTo(mymap)
+//         side_marker.on('click', function(e){
+//             e.target.removeFrom(mymap)
+//         });
+//     }
+
+// });
+currentMarkers = {}
+ // mymap.on('click', function (e) {
+ //    // if start and end selected and clicked map again start over
+ //    if (currentMarkers.start && currentMarkers.end) {
+ //      currentMarkers.start.remove();
+ //      currentMarkers.end.remove();
+ //      currentMarkers.start = null;
+ //      currentMarkers.end = null;      
+ //    }
+ //    var feat = L.marker(e.latlng, {
+ //        draggable: true
+ //    }).addTo(mymap)
+ //    // assign the marker to start/stop depending on state 
+ //    if (currentMarkers.start == null)
+ //      currentMarkers.start = feat;
+ //    else if (currentMarkers.end == null) {
+ //      currentMarkers.end = feat;
+ //    }
+ //    feat.on('click', function(e) {
+ //      // track the state of removal. we prob wanna use ids from the markers to target them
+ //      if (currentMarkers.start && currentMarkers.end == null) {
+ //        currentMarkers.start.remove();
+ //        currentMarkers.end.remove();
+ //        currentMarkers.start = null;
+ //        currentMarkers.end = null;
+ //      } 
+ //      else{
+ //        currentMarkers.end.remove();
+ //        currentMarkers.end = null;  
+ //      }
+ //      e.remove();
+ //    });
+ //  });
+ var myIcon = L.divIcon();
 // you can set .my-div-icon styles in CSS
-var start_point = true
-mymap.on('click', function (e) {
+ currentMarkersPCT = {}
+ currentMarkersSide = {}
+ mymap.on('click', function (e) {
     var _select   = document.getElementById("symbols");
     var _val      = _select.value;
     var layerName = _select.options[_select.selectedIndex].text;
     console.log("layer: ", layerName);
-
     if (layerName == "PCT Point"){
-        var pct_marker = L.marker(e.latlng, {
+        addRemoveMarker(e, currentMarkersPCT, layerName)
+    }
+    else {
+        addRemoveMarker(e, currentMarkersSide, layerName)
+    }
+ });
+function addRemoveMarker(event, currentMarkers, layerName) {
+// if start and end selected and clicked map again start over
+    if (currentMarkers.start && currentMarkers.end) {
+      currentMarkers.start.remove();
+      currentMarkers.end.remove();
+      currentMarkers.start = null;
+      currentMarkers.end = null;      
+    }
+    // Add different icon per type
+    if (layerName == "PCT Point"){
+        var feat = L.marker(event.latlng, {
         draggable: true
-        }).addTo(mymap)
-        var popup = L.popup()
-        .setLatLng(e.latlng)
-        .setContent('<p>Hello world!<br />This is a nice popup.</p>')
-        .openOn(mymap);
-        pct_marker.on('click', function(e){
-            e.target.removeFrom(mymap)
-        });
+    }).addTo(mymap)
     }
     else{
-        var side_marker = L.marker(e.latlng, {
+        var feat = L.marker(event.latlng, {
         draggable: true,
         icon: myIcon
-        }).addTo(mymap)
-        side_marker.on('click', function(e){
-            e.target.removeFrom(mymap)
-        });
+    }).addTo(mymap)
     }
 
-});
-
-function removeFeature(feature){
-    feature.remove()
+    // assign the marker to start/stop depending on state 
+    if (currentMarkers.start == null)
+      currentMarkers.start = feat;
+    else if (currentMarkers.end == null) {
+      currentMarkers.end = feat;
+    }
+    feat.on('click', function(e) {
+      // track the state of removal. we prob wanna use ids from the markers to target them
+      if (currentMarkers.start && currentMarkers.end == null) {
+        currentMarkers.start.remove();
+        currentMarkers.end.remove();
+        currentMarkers.start = null;
+        currentMarkers.end = null;
+      } 
+      else{
+        currentMarkers.end.remove();
+        currentMarkers.end = null;  
+      }
+      e.remove();
+    });
 }
 // pct_centerline.on("click", function(e) {
 //     var id = e.feature.id
